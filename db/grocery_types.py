eec3012ce71cd31bb3f1abc2fe00124048ba821b
Dorothy <dorothyzhang825@gmail.com>
2022-10-01 14:31:07 -0400

    added methods and main to module

--- db/grocery_types.py
+++ db/grocery_types.py
@@ -13,7 +13,10 @@ MEAT = 'Meat'
 
 """
 Ex: {BAKED_GOODS: {'bread': [Quantity you want,
-Quanitity you have, Expiration date1,[Notes]]}}
+Quanitity you have, Expiration date1, [Notes]]}}
+Design note: Index 3 in value list is designated
+as notes list. Will provide user ability to append
+string notes to the list, for freedom of use.
 """
 GROC_TYPES = {BAKED_GOODS: {},
               CARBS: {},
@@ -25,5 +28,18 @@ GROC_TYPES = {BAKED_GOODS: {},
               MEAT: {}}
 
 
-def get_grocery_types():
+def get_groc_types():
     return list(GROC_TYPES.keys())
+
+
+def get_groc_items_by_type(groc_type):
+    return GROC_TYPES.get(groc_type, None)
+
+
+def main():
+    groc_types = get_groc_types()
+    print(groc_types)
+
+
+if __name__ == "__main__":
+    main()
