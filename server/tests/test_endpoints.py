e41fb6750231e920292178d36ad7a8a408f81f82
Yeseon02 <yk2384@nyu.edu>
2022-10-02 23:04:33 -0400

    Added test for getting grocery by type

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -4,6 +4,7 @@ import pytest
 import server.endpoints as ep
 
 TEST_CLIENT = ep.app.test_client()
+TEST_GROC_TYPE = 'Meat'
 
 
 def test_hello():
@@ -16,3 +17,13 @@ def test_get_grocery_type_list():
     """
     resp_json = TEST_CLIENT.get(ep.GROC_TYPE_LIST).get_json()
     assert isinstance(resp_json[ep.GROC_TYPE_LIST_NM], list)
+    
+
+def test_get_grocery_type_detail():
+    """
+    Check if grocery detail is returned
+    """
+    resp_json = TEST_CLIENT.get(f'{ep.GROC_TYPE_DETAILS}/{TEST_GROC_TYPE}').get_json()
+    assert TEST_GROC_TYPE in resp_json
+    assert isinstance(resp_json[TEST_GROC_TYPE], dict)
+
