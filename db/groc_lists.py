ab841dd1b6ca4d22a81d7975ade6b2b438a51008
junhkim20 <jk7048@nyu.edu>
2022-10-09 16:40:47 -0400

    fixing dorothy's errors

--- db/groc_lists.py
+++ db/groc_lists.py
@@ -9,12 +9,12 @@ GROC_LISTS = {}
 
 REQUIRED_FIELDS = [USER_NAME, LIST_NAME, NUM_ITEMS, GROC_LISTS]
 lists = {USER_NAME: {LIST_NAME: 'trip1',
-                            NUM_ITEMS: 1,
-                            GROC_LISTS: {'itemA': '10-20-2022'}},
-                'user2': {LIST_NAME: 'trip2',
-                          NUM_ITEMS: 2,
-                          GROC_LISTS: {'itemB': '11-03-2022',
-                                       'itemC': '10-30-2022'}}}
+                     NUM_ITEMS: 1,
+                     GROC_LISTS: {'itemA': '10-20-2022'}},
+         'user2': {LIST_NAME: 'trip2',
+                   NUM_ITEMS: 2,
+                   GROC_LISTS: {'itemB': '11-03-2022',
+                                'itemC': '10-30-2022'}}}
 
 
 def get_usernames():
