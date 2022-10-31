3788e8c2993c3a035599b19a01446577c1de8d81
tse-jeff <jeff.tse@nyu.edu>
2022-10-30 23:34:25 -0400

    added get_grocery_details to groceries.py

--- db/groceries.py
+++ db/groceries.py
@@ -3,7 +3,7 @@ This module is for a singular grocery list structure. It contains functions
 necessary to interact with a grocery list.
 """
 
-# import groc_types as gtyp
+import groc_types as gtyp
 
 GROC_TYPE = 'grocery_type'
 Quantity = 'quantity'
@@ -13,7 +13,7 @@ REQUIRED_FIELDS = [GROC_TYPE, Quantity, EXPIRATION_DATE]
 # example of a grocery list structure
 grocery_list = {
     "item1": {
-        GROC_TYPE: None,
+        GROC_TYPE: gtyp.BAKED_GOODS,
         Quantity: "10",
         EXPIRATION_DATE: "10-20-2022"
     }
@@ -25,6 +25,13 @@ def get_grocery_items():
     returns a list of all items in the grocery list
     """
     return list(grocery_list.keys())
+
+
+def get_grocery_details(item):
+    """
+    returns a dictionary of details for a singular grocery item
+    """
+    return grocery_list[item]
 
 
 def main():
--- db/tests/test_groceries.py
+++ db/tests/test_groceries.py
@@ -3,6 +3,14 @@ Testing module for the groceries.py
 """
 import pytest
 import db.groceries as grocs
+import db.groc_types as gtyp
+
+TEST_GROCERY_NAME = "item1"
+TEST_GROCERY = {
+    grocs.GROC_TYPE: gtyp.BAKED_GOODS,
+    grocs.Quantity: "10",
+    grocs.EXPIRATION_DATE: "10-20-2022"
+}
 
 
 def test_get_grocery_items():
@@ -11,3 +19,12 @@ def test_get_grocery_items():
     """
     assert isinstance(grocs.get_grocery_items(), list)
     assert len(grocs.get_grocery_items()) > 0
+
+
+def test_get_grocery_details():
+    """
+    tests get_grocery_details()
+    """
+    assert isinstance(grocs.get_grocery_details(TEST_GROCERY_NAME), dict)
+    for field in grocs.REQUIRED_FIELDS:
+        assert field in grocs.get_grocery_details(TEST_GROCERY_NAME)
