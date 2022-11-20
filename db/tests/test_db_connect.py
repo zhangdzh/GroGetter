ee0e89cf203055ba3431dc6ea707d782a77acef8
Yeseon02 <yk2384@nyu.edu>
2022-11-20 18:15:00 -0500

    fetch one fixed test db_connect

--- db/tests/test_db_connect.py
+++ db/tests/test_db_connect.py
@@ -13,7 +13,7 @@ TEST_NAME = 'test'
 def temp_rec():
     dbc.connect_db()
     dbc.client[TEST_DB][TEST_COLLECT].insert_one({TEST_NAME: TEST_NAME})
-    # yield to our test function
+    # yield to test function
     yield
     dbc.client[TEST_DB][TEST_COLLECT].delete_one({TEST_NAME: TEST_NAME})
 
