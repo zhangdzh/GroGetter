500a95372fb5356e815759265e9479693bb3e867
Dorothy <dorothyzhang825@gmail.com>
2022-11-20 21:07:13 -0500

    reverting

--- db/tests/test_db_connect.py
+++ db/tests/test_db_connect.py
@@ -17,12 +17,3 @@ def temp_rec():
     yield
     dbc.client[TEST_DB][TEST_COLLECT].delete_one({TEST_NAME: TEST_NAME})
 
-
-def test_fetch_one(temp_rec):
-    ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
-    assert ret is not None
-
-
-def test_fetch_one_not_there(temp_rec):
-    ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: 'not a field value in db!'})
-    assert ret is None
