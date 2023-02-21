05a376eae7b4dd9a09943a5a0b9ad3d946f5b1f6
Dorothy <dorothyzhang825@gmail.com>
2023-02-21 14:29:22 -0500

    changed directories to match our PA setup

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ fi
 
 echo "SSHing to PythonAnywhere."
 sshpass -p $PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
-    cd GroGetter; ./rebuild.sh
+    ./rebuild.sh
 EOF
