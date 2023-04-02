35f798d47fe8097b29923db985df14307018e63b
tse-jeff <jeff.tse@nyu.edu>
2023-04-02 17:31:39 -0400

    made mongo only connect to remote db

--- db/db_connect.py
+++ db/db_connect.py
@@ -16,21 +16,19 @@ def connect_db():
     """
     Creates a uniform connection to the DB.
     """
+    # made the mongo client only connect to cloud because we don't have
+    # a local mongo server
     global client
     if client is None:
         print("No connection with client yet.")
-        if os.environ.get("LOCAL_MONGO", LOCAL) == CLOUD:
-            password = PW
-            if not password:
-                raise ValueError("Please set a password" +
-                                 "to use Mongo in the cloud")
-            print("Connecting to Mongo Cloud")
-            client = pm.MongoClient(f'mongodb+srv://swef22:{password}'
-                                    + '@cluster0.qfnmmli.mongodb.net/'
-                                    + '?retryWrites=true&w=majority')
-        else:
-            print("Connecting to MongoDB locally.")
-            client = pm.MongoClient()
+        password = PW
+        if not password:
+            raise ValueError("Please set a password" +
+                             "to use Mongo in the cloud")
+        print("Connecting to Mongo Cloud")
+        client = pm.MongoClient(f'mongodb+srv://swef22:{password}'
+                                + '@cluster0.qfnmmli.mongodb.net/'
+                                + '?retryWrites=true&w=majority')
 
 
 def del_one(collection, filt, db=GROC_DB):
